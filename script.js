528f2ca02f1062a4c5bbf6a60afa501c2b161d3a
Ayush GUpta <146961251+indayushg@users.noreply.github.com>
2025-03-20 19:27:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// var audio = document.createElement('audio');
+// audio.setAttribute('src','sound.mp3');
+// audio.loop=true;
+// audio.play();
+
+var audio =document.createElement('audio');
+audio.setAttribute('src','audio2.mp3');
+audio.loop =true;
+audio.play();
